c3f373c3d70ea66f9132e23c4109fe64d814fac0
anhle697 <32230670+anhle697@users.noreply.github.com>
2019-05-07 23:45:05 -0500

    Update createGUI.py

--- createGUI.py
+++ createGUI.py
@@ -6,20 +6,21 @@ int_window.title('Simple Integration Methods GUI')
 left_main_frame = Frame(int_window)
 right_main_frame = Frame(int_window)
 
-testL = Label(left_main_frame, text = "Left")
+result_label = Label(left_main_frame, text = "Result:", bd = 1, relief = "solid", width=12, anchor = W)
+error_label = Label(left_main_frame, text = "% Error:" , bd = 1, relief = "solid", width=12, anchor = W)
 
 functions_label = Label(right_main_frame, text = "Function:", bd = 1, relief = "solid", width=12, anchor = W)
+functions_entry = Entry(right_main_frame,)
 interval_label = Label(right_main_frame, text = "Intervals and n:", bd = 1, relief = "solid", width=12, anchor = W)
 method_label = Label(right_main_frame, text = "Methods:", bd = 1, relief = "solid", width=12, anchor = W)
-testL.grid(row = 0)
+
+result_label.grid(row = 0, padx=10, pady=10)
+error_label.grid(row = 1, padx=10, pady=10)
 functions_label.grid(row = 0, padx=10, pady=10)
 interval_label.grid(row = 1, padx=10, pady=10)
 method_label.grid(row = 2,padx=10, pady=10)
 
 left_main_frame.grid(row = 0, column = 0)
 right_main_frame.grid(row = 0, column = 1)
-
-
-
 
 int_window.mainloop()
