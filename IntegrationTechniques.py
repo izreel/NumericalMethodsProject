e6a50a09cff2a05b54b6f7e3fe63d0099d1096f0
Israel <israpere@gmail.com>
2019-05-09 21:42:19 -0500

    changes to integration techniques

--- IntegrationTechniques.py
+++ IntegrationTechniques.py
@@ -3,25 +3,36 @@ from numpy import cos, sin, tan, arccos, arcsin, arctan, exp, pi, sqrt
 from scipy.misc import derivative
 
 
-def funct(function_string, x):
-    return eval(function_string)
-
 class Integration:
+    def __init__(self):
+        self.function_string = ''
+
+    def set_function_string(self, input_function):
+        self.function_string = input_function
+
+    def funct(self, x):
+        return eval(self.function_string)
+
     def midpoint_rule(self, a, b, f, n):
-        h = (b-a)/(n+2)
-        error = (h**3) / 3 * derivative(funct, (b + a) / 2, dx=0.1, n=2)
+        self.set_function_string(f)
+        h = (b - a) / (n + 2)
+        error = (h ** 3) / 3 * derivative(self.funct, (b + a) / 2, dx=0.1, n=2, order=3)
+
+        return 2 * h * self.funct((a + b) / 2), error
 
     def trapezoidal_rule(self, a, b, f):
+        self.set_function_string(f)
         h = b - a
-        error = -(h ** 3) / 12 * derivative(funct, (b + a) / 2, dx=0.1, n=2)
+        error = -(h ** 3) / 12 * derivative(self.funct, (b + a) / 2, dx=0.1, n=2, order=3)
 
-        return (h/2)*(funct(f, a) + funct(f, b)), error
+        return (h / 2) * (self.funct(a) + self.funct(b)), error
 
     def simpson_rule(self, a, b, f):
-        h = (b-a)/2
-        error = -(h**5) / 90 * derivative(funct, (b + a) / 2, dx=0.1, n=4)
+        self.set_function_string(f)
+        h = (b - a) / 2
+        error = -(h ** 5) / 90 * derivative(self.funct, (b + a) / 2, dx=0.1, n=4, order=5)
 
-        return (h/3)*(funct(f, a) + 4 * funct(f, a + h) + funct(f, b)), error
+        return (h / 3) * (self.funct(a) + 4 * self.funct(a + h) + self.funct(b)), error
 
     def composite_midpoint_rule(self, a, b, f, n):
         '''Performs the Midpoint Rule or Rectangular Integration on interval [a,b]
@@ -29,14 +40,15 @@ class Integration:
             :param n: number of subintervals in [a,b]
             :return: integration of function f on interval [a,b]
         '''
+        self.set_function_string(f)
         h = (b - a) / (n + 2)
         final_sum = 0
 
-        for i in range(0, int(n/2)+1):
+        for i in range(0, int(n / 2) + 1):
             x = a + i * h
-            final_sum += funct(f, x)
+            final_sum += self.funct(x)
 
-        error = ((b-a)/6) * (h**2) * derivative(funct, (b+a)/2, dx= 0.1, n=2)
+        error = ((b - a) / 6) * (h ** 2) * derivative(self.funct, (b + a) / 2, dx=0.1, n=2, order=3)
         return final_sum * h, error
 
     def composite_trapezoidal_rule(self, a, b, f, n):
@@ -45,19 +57,19 @@ class Integration:
             :param n: number of subintervals in [a,b]
             :return: integration of function f on interval [a,b]
         '''
+        self.set_function_string(f)
         h = (b - a) / n
-        temp_xa = funct(f, a)
-        temp_xb = funct(f, b)
+        temp_xa = self.funct(a)
+        temp_xb = self.funct(b)
 
         final_sum = (temp_xa + temp_xb)
 
         for i in range(1, n):
             x = a + i * h
-            final_sum += funct(f, x)
+            final_sum += self.funct(x)
 
-        error = -((b-a)/12) * (h**2) * derivative(funct, (b + a) / 2, dx=0.1, n=2)
-        return final_sum * h/2, error
-
+        error = -((b - a) / 12) * (h ** 2) * derivative(self.funct, (b + a) / 2, dx=0.1, n=2, order=3)
+        return final_sum * h / 2, error
 
     def composite_simpson(self, a, b, f, n):
         '''Performs Composite Simpson technique on interval [a,b]
@@ -65,9 +77,11 @@ class Integration:
             :param n: number of subintervals in [a,b]
             :return: integration of function f on interval [a,b]
         '''
+        self.set_function_string(f)
+
         h = (b - a) / n
-        temp_xa = funct(f, a)
-        temp_xb = funct(f, b)
+        temp_xa = self.funct(a)
+        temp_xb = self.funct(b)
         x0 = temp_xa + temp_xb
         x1 = 0
         x2 = 0
@@ -75,12 +89,9 @@ class Integration:
         for i in range(n):
             x = a + i * h
             if i % 2 == 0:
-                x2 += funct(f, x)
+                x2 += self.funct(x)
             else:
-                x1 += funct(f, x)
+                x1 += self.funct(x)
 
-        error =  -(b-a)/180 * (h**4) * derivative(funct, (b+a)/2, dx= 0.1, n=4)
+        error = -(b - a) / 180 * (h ** 4) * derivative(self.funct, float((b + a) / 2), dx=0.1, n=4, order=5)
         return h * (x0 + 2 * x2 + 4 * x1) / 3, error
-
-    
-
--- TestIntegrationMethods.py
+++ TestIntegrationMethods.py
@@ -1,28 +1,28 @@
 import numpy as np
 from IntegrationTechniques import Integration
 
-def f(x):
-    return x
+def f():
+    return 'x'
 
-def g(x):
-    return x**2 + 2*x
+def g():
+    return 'x**2 + 2*x'
 
-def h(x):
-    return np.cos(x) + 1/2
+def h():
+    return 'cos(x) + 1/2'
 
 def main():
     integration = Integration()
     print(50)
-    print(integration.composite_simpson(0, 10, f, 10))
+    print(integration.composite_simpson(0, 10, f(), 10))
 
     print(10**3/3 + 100)
-    print(integration.composite_simpson(0, 10, g, 10))
+    print(integration.composite_simpson(0, 10, g(), 10))
 
-    print(integration.composite_simpson(0, np.pi/2, h, 10))
+    print(integration.composite_simpson(0, np.pi/2, h(), 10))
     #print(integration.simpson_rule(0, np.pi/2, h, 9))
 
-    print("User Input")
-    x = 2
-    funct = eval(input("Please input a function(x): "))
-
-    print(funct)
+    # print("User Input")
+    # x = 2
+    # funct = eval(input("Please input a function(x): "))
+    #
+    # print(funct)
